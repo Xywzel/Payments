45c753673011a467c3f5326281e851b6e7c8070c
Oskari Nousiainen <oskari.nousiainen@gmail.com>
2015-01-08 12:44:14 +0200

    Renamed some test files

--- src/test/scala/PaymentSpec.scala
+++ src/test/scala/PaymentSpec.scala
@@ -3,7 +3,7 @@ import org.scalatest._
 /**
  * Created by Oskari on 8.1.2015.
  */
-class PaymentTest extends FlatSpec with Matchers {
+class PaymentSpec extends FlatSpec with Matchers {
   "A cents to print" should "return correct printing for default options" in {
     Payment.centsToPrint(0) should equal("$0.00")
     Payment.centsToPrint(100) should equal("$1.00")
